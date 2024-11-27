ef51806db85cc94af9a4d89ddff50ec0a24905f0
GitHub Action <action@github.com>
2024-11-27 09:29:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/earlofego/jetpackcomposepreviewcreator/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/earlofego/jetpackcomposepreviewcreator/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.earlofego.jetpackcomposepreviewcreator.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
